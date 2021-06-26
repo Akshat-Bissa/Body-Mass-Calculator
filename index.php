8bce04d042bdd4d2f8a2412343bb58657e6daeb6
Akshat bissa <akshatbissa101006@gmail.com>
2021-06-26 11:35:53 +0530

    BODY-MASS-INDEX-CALCULATOR

---
+++ index.php
@@ -0,0 +1,22 @@
+<h1 align="center">Body Mass Index Calculator</h1>
+<form action="store.php" method="post">
+<table border="1px" colspan="1" align="center">
+<thead>
+<tr><th>Body Mass Index Calculator</th></tr>
+</thead>
+<tbody>
+<tr><td align="center">
+<input type="float" name="weight" id="we_id" placeholder="Enter your Weight in Kg" required>
+</td></tr>
+<tr><td align="center">
+<input type="float" name="hight" id="hi_id" placeholder="Enter your Hight in Metres" required>
+</td></tr>
+<tr><td align="center"><button>Submit</button></td></tr>
+</tbody>
+</table>
+
+
+
+
+
+</form>
